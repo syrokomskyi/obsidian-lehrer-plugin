6f25ce5e4f4de83d69a1ae9eaebaaeb15b11ae63
Andrii Syrokomskyi <signmotion@gmail.com>
2025-02-06 12:36:33 +0100

    Refactoring.

--- main.ts
+++ main.ts
@@ -3,25 +3,27 @@ import { Plugin} from 'obsidian';
 export default class CsvCodeBlockPlugin extends Plugin {
 	
 	async onload() {
-		this.registerMarkdownCodeBlockProcessor("lang-split-by-sentences", (source, el, ctx) => {
-			const wrapper = el.createEl("div", {
-				cls: "lehrer"
-			});
-			const table = wrapper.createEl("table");
-			const body = table.createEl("tbody");
-			const rows = source.split(/((?<=[.!?])\s)|\s+(?=[.!?])/).filter((row) => row.trim().length > 0);
-			for (let i = 0; i < rows.length; ++i) {
-				const row = body.createEl("tr");
-				row.createEl("td", { text: `${i + 1}`, cls: "number" });
-				row.createEl("td", { 
-					text: rows[i].trim(),
-					cls: "original-text"
-				});
-				row.createEl("td", { 
-					text: '',
-					cls: "input-text"
-				});
-			}
+		this.registerMarkdownCodeBlockProcessor("lang-split-by-sentences", processSplitBySentences);
+	}
+}
+
+function processSplitBySentences(source: string, el: HTMLElement, ctx: any): void {
+	const wrapper = el.createEl("div", {
+		cls: "lehrer"
+	});
+	const table = wrapper.createEl("table");
+	const body = table.createEl("tbody");
+	const rows = source.split(/((?<=[.!?])\s)|\s+(?=[.!?])/).filter((row) => row.trim().length > 0);
+	for (let i = 0; i < rows.length; ++i) {
+		const row = body.createEl("tr");
+		row.createEl("td", { text: `${i + 1}`, cls: "number" });
+		row.createEl("td", { 
+			text: rows[i].trim(),
+			cls: "original-text"
 		});
-	}	
+		row.createEl("td", { 
+			text: '',
+			cls: "input-text"
+		});
+	}
 }
