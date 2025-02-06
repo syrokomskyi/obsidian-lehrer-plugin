e4e791f1173f95786d933e8ae7321f3463551019
Andrii Syrokomskyi <signmotion@gmail.com>
2025-02-06 12:28:26 +0100

    Added number of rows and styles for table.

--- main.ts
+++ main.ts
@@ -4,14 +4,23 @@ export default class CsvCodeBlockPlugin extends Plugin {
 	
 	async onload() {
 		this.registerMarkdownCodeBlockProcessor("lang-split-by-sentences", (source, el, ctx) => {
-			const table = el.createEl("table");
+			const wrapper = el.createEl("div", {
+				cls: "lehrer"
+			});
+			const table = wrapper.createEl("table");
 			const body = table.createEl("tbody");
 			const rows = source.split(/((?<=[.!?])\s)|\s+(?=[.!?])/).filter((row) => row.trim().length > 0);
 			for (let i = 0; i < rows.length; ++i) {
 				const row = body.createEl("tr");
-				row.createEl("td", { text: `${i + 1}` });
-				row.createEl("td", { text: rows[i].trim() });
-				row.createEl("td", { text: '' });
+				row.createEl("td", { text: `${i + 1}`, cls: "number" });
+				row.createEl("td", { 
+					text: rows[i].trim(),
+					cls: "original-text"
+				});
+				row.createEl("td", { 
+					text: '',
+					cls: "input-text"
+				});
 			}
 		});
 	}	
