5c52f02ebe7211f36309575ac055c9f01459cd4a
Andrii Syrokomskyi <signmotion@gmail.com>
2025-03-22 11:36:50 +0100

    Initialized a session with fingerprint device.

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
+import { fingerprint } from "@webgogol/core-share";
+import { $session } from "lib/store/state";
 import { Plugin } from "obsidian";
 import { process } from "./lib/process";
 
-export default class CsvCodeBlockPlugin extends Plugin {
+export default class ObsidianPlugin extends Plugin {
   async onload() {
+    const session = await fingerprint();
+    console.log("onload()", { session });
+    $session.set(session);
+
     this.registerMarkdownCodeBlockProcessor("lang", process);
   }
 }
